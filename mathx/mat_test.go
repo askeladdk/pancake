1d4335a588e301b0dc18f41c711bf3fe269351fc
askeladdk <askeladdk@users.noreply.github.com>
2020-06-19 23:53:39 +0200

    Bugfix matrix tests

--- mathx/mat_test.go
+++ mathx/mat_test.go
@@ -2,15 +2,15 @@ package mathx
 
 import "testing"
 
-func Test_Ident3Inv(t *testing.T) {
-	m := Ident3()
+func Test_IdentAff3_Inv(t *testing.T) {
+	m := IdentAff3()
 	i := m.Inv()
 	if i != m {
 		t.Fatal("m!=i")
 	}
 }
 
-func Test_Aff3Inv(t *testing.T) {
+func Test_Aff3_Inv(t *testing.T) {
 	m := Aff3{1, 2, 3, 4, 5, 6}
 	i := m.Inv().Inv()
 	if i != m {
